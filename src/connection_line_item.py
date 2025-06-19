b53f508685feead14005d919a80e226ac4fc5eb0
tomasarrufat <tomas.arrufat@gmail.com>
2025-06-19 10:37:11 +0200

    Fix connection line item inheritance

--- src/connection_line_item.py
+++ src/connection_line_item.py
@@ -1,11 +1,11 @@
-from PyQt5.QtWidgets import QGraphicsLineItem, QGraphicsItem
-from PyQt5.QtGui import QPen, QColor
-from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5.QtWidgets import QGraphicsObject, QGraphicsItem
+from PyQt5.QtGui import QPen, QColor, QPainter
+from PyQt5.QtCore import Qt, pyqtSignal, QRectF, QLineF
 
 from .info_area_item import InfoAreaItem
 from . import utils
 
-class ConnectionLineItem(QGraphicsLineItem):
+class ConnectionLineItem(QGraphicsObject):
     """Graphics item representing a connection line between two info areas."""
 
     item_selected = pyqtSignal(QGraphicsItem)
@@ -17,13 +17,16 @@ class ConnectionLineItem(QGraphicsLineItem):
         self.item_map = item_map
         self.setFlags(QGraphicsItem.ItemIsSelectable)
         self.setZValue(self.config_data.get('z_index', utils.Z_VALUE_INFO_RECT))
+        self._line = QLineF()
+        self._pen = QPen()
         self._update_pen()
         self.update_position()
 
     def _update_pen(self):
         color = QColor(self.config_data.get('line_color', '#00ffff'))
         thickness = self.config_data.get('thickness', 2)
-        self.setPen(QPen(color, thickness))
+        self._pen = QPen(color, thickness)
+        self.update()
 
     def update_position(self):
         src_item = self.item_map.get(self.config_data.get('source'))
@@ -31,7 +34,17 @@ class ConnectionLineItem(QGraphicsLineItem):
         if isinstance(src_item, InfoAreaItem) and isinstance(dst_item, InfoAreaItem):
             src_center = src_item.mapToScene(src_item.boundingRect().center())
             dst_center = dst_item.mapToScene(dst_item.boundingRect().center())
-            self.setLine(src_center.x(), src_center.y(), dst_center.x(), dst_center.y())
+            self.prepareGeometryChange()
+            self._line = QLineF(src_center, dst_center)
+            self.update()
+
+    def boundingRect(self):
+        extra = self._pen.widthF() / 2
+        return QRectF(self._line.p1(), self._line.p2()).normalized().adjusted(-extra, -extra, extra, extra)
+
+    def paint(self, painter, option, widget=None):
+        painter.setPen(self._pen)
+        painter.drawLine(self._line)
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
